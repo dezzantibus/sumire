75bd4b9439b572b73ed639c27b05ce751ee91538
Andrea Passante <zante@inwind.it>
2020-04-24 22:01:43 +0200

    Duh… forgot to actually run it

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ require_once __DIR__ . '/tools/class_finder.php';
 spl_autoload_register( 'class_finder::getClassFile' );
 
 /** @var handler $page */
-//$page = router::run();
+$page = router::run();
 //$page->run();
