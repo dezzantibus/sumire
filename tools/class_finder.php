15f53a690167d835bcb5464da863b1683dae687b
Andrea Passante <zante@inwind.it>
2020-04-25 11:39:20 +0200

    Missing some routing I really must set up that local server…

--- tools/class_finder.php
+++ tools/class_finder.php
@@ -10,9 +10,13 @@ class class_finder
 
         switch( $frags[0] )
         {
-            case 'layout':   self::getLayoutClass( $name, $frags );       break;
-            case 'handler':  self::getHandlerClass( $name, $frags );      break;
-            case 'action':   self::getActionClass( $name, $frags );       break;
+            case 'layout':  self::getLayoutClass( $name, $frags );  break;
+            case 'handler': self::getHandlerClass( $name, $frags ); break;
+            case 'action':  self::getActionClass( $name, $frags );  break;
+
+            case 'html':     require_once __DIR__ . '/../layout/html/'     . $name . '.php'; break;
+            case 'elements': require_once __DIR__ . '/../layout/elements/' . $name . '.php'; break;
+
             case 'banner':   require_once __DIR__ . '/banner.php';        break;
             case 'constant': require_once __DIR__ . '/constant.php';      break;
             case 'message':  require_once __DIR__ . '/message.php';       break;
