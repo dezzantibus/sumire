8b8c67a488b7f38af56573ac820e2b71c7dfe45d
Andrea Passante <zante@inwind.it>
2020-04-27 16:32:26 +0200

    Poke

--- layout/admin/layout_admin_page.php
+++ layout/admin/layout_admin_page.php
@@ -52,33 +52,33 @@ abstract class layout_admin_page extends layout
             '<script src="/js/plugins/flot/jquery.flot.symbol.js"></script>',
             '<script src="/js/plugins/flot/jquery.flot.time.js"></script>',
 
-/*
+            //<!-- Peity -->
+            '<script src="/js/plugins/peity/jquery.peity.min.js"></script>',
+            '<script src="/js/demo/peity-demo.js"></script>',
 
-            <!-- Peity -->
-            <script src="/js/plugins/peity/jquery.peity.min.js"></script>
-            <script src="/js/demo/peity-demo.js"></script>
+            //<!-- Custom and plugin javascript -->
+            '<script src="/js/inspinia.js"></script>',
+            '<script src="/js/plugins/pace/pace.min.js"></script>',
 
-            <!-- Custom and plugin javascript -->
-            <script src="/js/inspinia.js"></script>
-            <script src="/js/plugins/pace/pace.min.js"></script>
+    /*
 
-            <!-- jQuery UI -->
-            <script src="/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+                <!-- jQuery UI -->
+                <script src="/js/plugins/jquery-ui/jquery-ui.min.js"></script>
 
-            <!-- Jvectormap -->
-            <script src="/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
-            <script src="/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+                <!-- Jvectormap -->
+                <script src="/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+                <script src="/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 
-            <!-- EayPIE -->
-            <script src="/js/plugins/easypiechart/jquery.easypiechart.js"></script>
+                <!-- EayPIE -->
+                <script src="/js/plugins/easypiechart/jquery.easypiechart.js"></script>
 
-            <!-- Sparkline -->
-            <script src="/js/plugins/sparkline/jquery.sparkline.min.js"></script>
+                <!-- Sparkline -->
+                <script src="/js/plugins/sparkline/jquery.sparkline.min.js"></script>
 
-            <!-- Sparkline demo data  -->
-            <script src="/js/demo/sparkline-demo.js"></script>',
+                <!-- Sparkline demo data  -->
+                <script src="/js/demo/sparkline-demo.js"></script>',
 
-            */
+                */
 
         '</body></html>';
     }
