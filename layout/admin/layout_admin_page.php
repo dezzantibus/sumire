6ea2739a39635d3c1bfab6038f2817361ddfa495
Andrea Passante <zante@inwind.it>
2020-04-27 16:31:04 +0200

    and another poke

--- layout/admin/layout_admin_page.php
+++ layout/admin/layout_admin_page.php
@@ -43,16 +43,16 @@ abstract class layout_admin_page extends layout
             '<script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>',
             '<script src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>',
 
-        /*
+            //<!-- Flot -->
+            '<script src="/js/plugins/flot/jquery.flot.js"></script>',
+            '<script src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>',
+            '<script src="/js/plugins/flot/jquery.flot.spline.js"></script>',
+            '<script src="/js/plugins/flot/jquery.flot.resize.js"></script>',
+            '<script src="/js/plugins/flot/jquery.flot.pie.js"></script>',
+            '<script src="/js/plugins/flot/jquery.flot.symbol.js"></script>',
+            '<script src="/js/plugins/flot/jquery.flot.time.js"></script>',
 
-            <!-- Flot -->
-            <script src="/js/plugins/flot/jquery.flot.js"></script>
-            <script src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-            <script src="/js/plugins/flot/jquery.flot.spline.js"></script>
-            <script src="/js/plugins/flot/jquery.flot.resize.js"></script>
-            <script src="/js/plugins/flot/jquery.flot.pie.js"></script>
-            <script src="/js/plugins/flot/jquery.flot.symbol.js"></script>
-            <script src="/js/plugins/flot/jquery.flot.time.js"></script>
+/*
 
             <!-- Peity -->
             <script src="/js/plugins/peity/jquery.peity.min.js"></script>
