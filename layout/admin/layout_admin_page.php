85e0b5504cb1e51f6b5a2f34197d7c5dfc82819e
Andrea Passante <zante@inwind.it>
2020-04-28 12:23:22 +0200

    Ok, let’ssee if this fixes the .html issue once and for all

--- layout/admin/layout_admin_page.php
+++ layout/admin/layout_admin_page.php
@@ -18,11 +18,11 @@ abstract class layout_admin_page extends layout
 
     <title>', $this->title ,'</title>
 
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css_admin/bootstrap.min.css" rel="stylesheet">
     <link href="/font-awesome/css/font-awesome.css" rel="stylesheet">
 
-    <link href="/css/animate.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css_admin/animate.css" rel="stylesheet">
+    <link href="/css_admin/style.css" rel="stylesheet">
 
 </head>
 
@@ -38,43 +38,43 @@ abstract class layout_admin_page extends layout
                 '</div>',
 
                 // <!-- Mainly scripts -->
-                '<script src="/js/jquery-2.1.1.js"></script>',
-                '<script src="/js/bootstrap.min.js"></script>',
-//                '<script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>',
-                '<script src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>',
+                '<script src="/js_admin/jquery-2.1.1.js"></script>',
+                '<script src="/js_admin/bootstrap.min.js"></script>',
+//                '<script src="/js_admin/plugins/metisMenu/jquery.metisMenu.js"></script>',
+                '<script src="/js_admin/plugins/slimscroll/jquery.slimscroll.min.js"></script>',
 
                 //<!-- Flot -->
-                '<script src="/js/plugins/flot/jquery.flot.js"></script>',
-                '<script src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>',
-                '<script src="/js/plugins/flot/jquery.flot.spline.js"></script>',
-                '<script src="/js/plugins/flot/jquery.flot.resize.js"></script>',
-                '<script src="/js/plugins/flot/jquery.flot.pie.js"></script>',
-                '<script src="/js/plugins/flot/jquery.flot.symbol.js"></script>',
-                '<script src="/js/plugins/flot/jquery.flot.time.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.tooltip.min.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.spline.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.resize.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.pie.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.symbol.js"></script>',
+                '<script src="/js_admin/plugins/flot/jquery.flot.time.js"></script>',
 
                 //<!-- Peity -->
-                '<script src="/js/plugins/peity/jquery.peity.min.js"></script>',
-                '<script src="/js/demo/peity-demo.js"></script>',
+                '<script src="/js_admin/plugins/peity/jquery.peity.min.js"></script>',
+                '<script src="/js_admin/demo/peity-demo.js"></script>',
 
                 //<!-- Custom and plugin javascript -->
-                '<script src="/js/inspinia.js"></script>',
-                '<script src="/js/plugins/pace/pace.min.js"></script>',
+                '<script src="/js_admin/inspinia.js"></script>',
+                '<script src="/js_admin/plugins/pace/pace.min.js"></script>',
 
                 //<!-- jQuery UI -->
-                '<script src="/js/plugins/jquery-ui/jquery-ui.min.js"></script>',
+                '<script src="/js_admin/plugins/jquery-ui/jquery-ui.min.js"></script>',
 
                 //<!-- Jvectormap -->
-                '<script src="/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>',
-                '<script src="/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>',
+                '<script src="/js_admin/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>',
+                '<script src="/js_admin/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>',
 
                 //<!-- EayPIE -->
-                '<script src="/js/plugins/easypiechart/jquery.easypiechart.js"></script>',
+                '<script src="/js_admin/plugins/easypiechart/jquery.easypiechart.js"></script>',
 
                 //<!-- Sparkline -->
-                '<script src="/js/plugins/sparkline/jquery.sparkline.min.js"></script>',
+                '<script src="/js_admin/plugins/sparkline/jquery.sparkline.min.js"></script>',
 
                 //<!-- Sparkline demo data  -->
-                '<script src="/js/demo/sparkline-demo.js"></script>',
+                '<script src="/js_admin/demo/sparkline-demo.js"></script>',
 
         '</body></html>';
     }
