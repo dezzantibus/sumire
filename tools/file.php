76b42a1c03beee6b3a01818c9dbde6def85198d4
Andrea Passante <zante@inwind.it>
2020-04-30 19:02:38 +0200

    Ok, if this doesn’t work I’ll go and make dinner

--- tools/file.php
+++ tools/file.php
@@ -17,15 +17,20 @@ class file
             return false;
         }
 
-        $localFile  = $_FILES[ $input ]['tmp_name'];
+        $localFile  = '/var/www/temp/' .  $_FILES[ $input ]['name'];
         $remoteFile = $path . '/' . $_FILES[ $input ]['name'];
+
+        move_uploaded_file( $_FILES[ $input ]['tmp_name'], $localFile );
 
         $s3 = new S3( constant::KEY, constant::SECRET );
 
         $s3->putObjectFile( $localFile, constant::BUCKET, $remoteFile, S3::ACL_PUBLIC_READ, array(), 'image/jpeg' );
 
+        unlink( $localFile );
+
         return 'images.sumire.it/'. $remoteFile;
 
     }
+
 
 }
