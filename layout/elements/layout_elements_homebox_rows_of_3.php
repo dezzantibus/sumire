0e1a1dc6a69bae25e3306b14ff2af73327eebaf0
Andrea Passante <zante@inwind.it>
2020-05-15 19:32:31 +0200

    Fixed the rows of 3

--- layout/elements/layout_elements_homebox_rows_of_3.php
+++ layout/elements/layout_elements_homebox_rows_of_3.php
@@ -14,15 +14,16 @@ class layout_elements_homebox_rows_of_3 extends layout
     function render()
     {
         echo
-        //<!-- Post Left Sidebar Start -->
-        '<div class="fbt-col-lg-3 col-md-12 post-left-sidebar clearfix">',
+        //<!-- Left Sidebar Start -->
+        '<div class="fbt-col-lg-3 col-md-12 left-sidebar">',
             '<div class="theiaStickySidebar">',
-                '<div class="widget fbt-vc-hr-inner clearfix">',
-                    '<div class="title-wrapper border-8">',
-                        '<h2><span class="color-8">', $this->category->category, '</span></h2>',
+                '<div class="widget fbt-vc-hr-inner">',
+                    '<div class="title-wrapper border-6">',
+                        '<h2><span class="color-6">', $this->category->category, '</span></h2>',
                     '</div>',
-                    '<div class="post-col-left clearfix">',
+                    '<div class="post-col-left">',
                         '<div class="row">';
+
 
                             if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
                             if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
@@ -37,7 +38,7 @@ class layout_elements_homebox_rows_of_3 extends layout
                 '</div>',
             '</div>',
         '</div>';
-        //<!-- Post Left Sidebar End -->
+        //<!-- Left Sidebar End -->
 
     }
 
@@ -49,27 +50,19 @@ class layout_elements_homebox_rows_of_3 extends layout
         $article = $this->category->homepage_articles->first();
 
         echo
-        '<div class="fbt-col-lg-12 col-md-4 col-xs-6 padding-reset">',
-            '<div class="post-item clearfix">',
-                '<div class="img-thumb">',
-                    '<a href="', $article->link(), '"><div class="fbt-resize" style="background-image: url(', constant::IMAGES_DOMAIN, $article->image1, ')"></div></a>',
-                '</div>',
-                '<div class="post-content">',
-                    '<a href="', $article->link(), '"><h3>', $article->title, '</h3></a>',
-                    '<div class="post-info">',
-                        '<span>', $article->dateForDisplay( $article->date ), '</span>',
-                        //'<span>-</span>',
-                        //'<span class="rating">',
-                            //'<i class="fa fa-star"></i>',
-                            //'<i class="fa fa-star"></i>',
-                            //'<i class="fa fa-star"></i>',
-                            //'<i class="fa fa-star"></i>',
-                            //'<i class="fa fa-star"></i>',
-                        //'</span>',
-                    '</div>',
-                '</div>',
-            '</div>',
-        '</div>';
+        '<div class="fbt-col-lg-12 col-md-4 col-xs-6 padding-reset">
+            <div class="post-item clearfix">
+                <div class="img-thumb">
+                    <a href="', $article->link(), '"><div class="fbt-resize" style="background-image: url(', constant::IMAGES_DOMAIN, $article->image1, ')"></div></a>
+                </div>
+                <div class="post-content">
+                    <a href="', $article->link(), '"><h3>', $article->title, '</h3></a>
+                    <div class="post-info">
+                        <span>', $article->dateForDisplay( $article->date ), '</span>
+                    </div>
+                </div>
+            </div>
+        </div>';
 
 
 
