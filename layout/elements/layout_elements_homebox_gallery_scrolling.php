5aa30a8584af9043437d38a915b585b9c5d4b3dc
Andrea Passante <zante@inwind.it>
2020-05-15 19:46:48 +0200

    Two more homebodies

--- layout/elements/layout_elements_homebox_gallery_scrolling.php
+++ layout/elements/layout_elements_homebox_gallery_scrolling.php
@@ -13,85 +13,51 @@ class layout_elements_homebox_gallery_scrolling extends layout
 
     function render()
     {
-        echo '
-								<!-- Main Carousel Start -->
-								<div class="main-carousel">
-									<div class="title-wrapper border-6">
-										<h2><span class="color-6">Landscapes</span></h2>
-									</div>
-									<div class="carousel-content-box owl-wrapper clearfix">
-										<div class="owl-carousel" data-num="3">
-											<div class="item fbt-hr-crs">
-												<div class="post-item clearfix">
-													<div class="img-thumb">
-														<a href="single.html"><div class="fbt-resize" style="background-image: url(http://placekitten.com/640/427)"></div></a>
-														<div class="img-credits">
-															<h3>China\'s drive to become a rugby union superpower.</h3>
-															<div class="post-info">
-																<span>Sep 26, 2016</span>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="item fbt-hr-crs">
-												<div class="post-item clearfix">
-													<div class="img-thumb">
-														<a href="single.html"><div class="fbt-resize" style="background-image: url(http://placekitten.com/640/427)"></div></a>
-														<div class="img-credits">
-															<h3>The age of first-time mothers is rising faster in the US.</h3>
-															<div class="post-info">
-																<span>Sep 26, 2016</span>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="item fbt-hr-crs">
-												<div class="post-item clearfix">
-													<div class="img-thumb">
-														<a href="single.html"><div class="fbt-resize" style="background-image: url(http://placekitten.com/640/427)"></div></a>
-														<div class="img-credits">
-															<h3>Nam iusto delicata ne, eam dolore singulis maiestatis ex.</h3>
-															<div class="post-info">
-																<span>Sep 26, 2016</span>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="item fbt-hr-crs">
-												<div class="post-item clearfix">
-													<div class="img-thumb">
-														<a href="single.html"><div class="fbt-resize" style="background-image: url(http://placekitten.com/640/427)"></div></a>
-														<div class="img-credits">
-															<h3>Nam iusto delicata ne, eam dolore singulis maiestatis ex.</h3>
-															<div class="post-info">
-																<span>Sep 26, 2016</span>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="item fbt-hr-crs">
-												<div class="post-item clearfix">
-													<div class="img-thumb">
-														<a href="single.html"><div class="fbt-resize" style="background-image: url(http://placekitten.com/640/427)"></div></a>
-														<div class="img-credits">
-															<h3>Nam iusto delicata ne, eam dolore singulis maiestatis ex.</h3>
-															<div class="post-info">
-																<span>Sep 26, 2016</span>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div><!-- Main Carousel End -->
+        echo
+        //<!-- Main Carousel Start -->
+        '<div class="main-carousel">',
+            '<div class="title-wrapper border-6">',
+                '<h2><span class="color-6">', $this->category->category, '</span></h2>',
+            '</div>',
+            '<div class="carousel-content-box owl-wrapper clearfix">',
+                '<div class="owl-carousel" data-num="3">';
 
+                    if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
+                    if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
+                    if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
+                    if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
+                    if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
+                    if( !$this->category->homepage_articles->isEmpty() ) $this->itemBox();
 
-        ';
+                echo
+                '</div>',
+            '</div>',
+        '</div>';
+        //<!-- Main Carousel End -->
+
+    }
+
+    private function itemBox()
+    {
+
+        /** @var data_news_article $article */
+        $article = $this->category->homepage_articles->first();
+
+        echo
+        '<div class="item fbt-hr-crs">',
+            '<div class="post-item clearfix">',
+                '<div class="img-thumb">',
+                    '<a href="', $article->link(), '"><div class="fbt-resize" style="background-image: url(', constant::IMAGES_DOMAIN, $article->image1, ')"></div></a>',
+                    '<div class="img-credits">',
+                        '<h3>', $article->title, '</h3>',
+                        '<div class="post-info">',
+                            '<span>', $article->dateForDisplay( $article->date ), '</span>',
+                        '</div>',
+                    '</div>',
+                '</div>',
+            '</div>',
+        '</div>';
+
     }
 
 }
