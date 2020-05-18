12e412ede8de913f5a7e1b171f34b01206a95cf8
Andrea Passante <zante@inwind.it>
2020-05-18 19:09:12 +0200

    Fixed homebox rows of 3

--- layout/elements/layout_elements_homebox_rows_of_3.php
+++ layout/elements/layout_elements_homebox_rows_of_3.php
@@ -15,7 +15,7 @@ class layout_elements_homebox_rows_of_3 extends layout
     {
         echo
         //<!-- Left Sidebar Start -->
-        '<div class="fbt-col-lg-3 col-md-12 left-sidebar">',
+        '<div class="fbt-vc-inner nude large clearfix">',
             '<div class="theiaStickySidebar">',
                 '<div class="widget fbt-vc-hr-inner">',
                     '<div class="title-wrapper border-6">',
@@ -50,21 +50,19 @@ class layout_elements_homebox_rows_of_3 extends layout
         $article = $this->category->homepage_articles->first();
 
         echo
-        '<div class="fbt-col-lg-12 col-md-4 col-xs-6 padding-reset">
-            <div class="post-item clearfix">
-                <div class="img-thumb">
-                    <a href="', $article->link(), '"><div class="fbt-resize" style="background-image: url(', constant::IMAGES_DOMAIN, $article->image1, ')"></div></a>
-                </div>
-                <div class="post-content">
-                    <a href="', $article->link(), '"><h3>', $article->title, '</h3></a>
-                    <div class="post-info">
-                        <span>', $article->dateForDisplay( $article->date ), '</span>
-                    </div>
-                </div>
-            </div>
-        </div>';
-
-
+        '<div class="fbt-col-lg-12 col-md-4 col-xs-6 padding-reset">',
+            '<div class="post-item clearfix">',
+                '<div class="img-thumb">',
+                    '<a href="', $article->link(), '"><div class="fbt-resize" style="background-image: url(', constant::IMAGES_DOMAIN, $article->image1, ')"></div></a>',
+                '</div>',
+                '<div class="post-content">',
+                    '<a href="', $article->link(), '"><h3>', $article->title, '</h3></a>',
+                    '<div class="post-info">',
+                        '<span>', $article->dateForDisplay( $article->date ), '</span>',
+                    '</div>',
+                '</div>',
+            '</div>',
+        '</div>';
 
     }
 
