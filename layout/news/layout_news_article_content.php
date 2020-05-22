bf03a037f4c8c5bc108ea04b699471cf773cb50a
Andrea Passante <zante@inwind.it>
2020-05-22 07:02:01 +0200

    ok, news should be sorted now

--- layout/news/layout_news_article_content.php
+++ layout/news/layout_news_article_content.php
@@ -57,7 +57,7 @@ class layout_news_article_content extends layout
                 '<div class="clearfix"></div>',
 
                 '<div class="post-text-content clearfix">',
-                    '<p>', str_replace( "\n", '<br />', str_replace( "\n\n", '</p><p>', $this->article->text ) ), '</p>';
+                    '<p>', str_replace( "\r\n", '<br />', str_replace( "\r\n\r\n", '</p><p>', $this->article->text ) ), '</p>';
 
                     if( !empty( $this->article->source ) )
                     {
