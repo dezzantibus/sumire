cb2701df91b6c66f7274fed7e91ac37b1fb90184
Andrea Passante <zante@inwind.it>
2021-04-07 17:10:16 +0200

    Changed domain (and bucket) for Rie’s blog

--- handler/admin/blog/handler_admin_blog_article_save.php
+++ handler/admin/blog/handler_admin_blog_article_save.php
@@ -45,17 +45,19 @@ class handler_admin_blog_article_save extends handler_action
 
             $path = 'blog/' . $blog . '/' . date('Y-m-d') . '/' . $article->title;
 
-            if( !empty( $this->files['cover'] ) )   $article->cover   = file::saveFromPost( $this->files['cover'],   $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image1'] ) )  $article->image1  = file::saveFromPost( $this->files['image1'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image2'] ) )  $article->image2  = file::saveFromPost( $this->files['image2'],  $path, 'images.vivereadicomano.com');
-            if( !empty( $this->files['image3'] ) )  $article->image3  = file::saveFromPost( $this->files['image3'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image4'] ) )  $article->image4  = file::saveFromPost( $this->files['image4'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image5'] ) )  $article->image5  = file::saveFromPost( $this->files['image5'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image6'] ) )  $article->image6  = file::saveFromPost( $this->files['image6'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image7'] ) )  $article->image7  = file::saveFromPost( $this->files['image7'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image8'] ) )  $article->image8  = file::saveFromPost( $this->files['image8'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image9'] ) )  $article->image9  = file::saveFromPost( $this->files['image9'],  $path, 'images.vivereadicomano.com' );
-            if( !empty( $this->files['image10'] ) ) $article->image10 = file::saveFromPost( $this->files['image10'], $path, 'images.vivereadicomano.com' );
+            $bucket = 'images.vivereintoscana.it';
+
+            if( !empty( $this->files['cover'] ) )   $article->cover   = file::saveFromPost( $this->files['cover'],   $path, $bucket );
+            if( !empty( $this->files['image1'] ) )  $article->image1  = file::saveFromPost( $this->files['image1'],  $path, $bucket );
+            if( !empty( $this->files['image2'] ) )  $article->image2  = file::saveFromPost( $this->files['image2'],  $path, $bucket );
+            if( !empty( $this->files['image3'] ) )  $article->image3  = file::saveFromPost( $this->files['image3'],  $path, $bucket );
+            if( !empty( $this->files['image4'] ) )  $article->image4  = file::saveFromPost( $this->files['image4'],  $path, $bucket );
+            if( !empty( $this->files['image5'] ) )  $article->image5  = file::saveFromPost( $this->files['image5'],  $path, $bucket );
+            if( !empty( $this->files['image6'] ) )  $article->image6  = file::saveFromPost( $this->files['image6'],  $path, $bucket );
+            if( !empty( $this->files['image7'] ) )  $article->image7  = file::saveFromPost( $this->files['image7'],  $path, $bucket );
+            if( !empty( $this->files['image8'] ) )  $article->image8  = file::saveFromPost( $this->files['image8'],  $path, $bucket );
+            if( !empty( $this->files['image9'] ) )  $article->image9  = file::saveFromPost( $this->files['image9'],  $path, $bucket );
+            if( !empty( $this->files['image10'] ) ) $article->image10 = file::saveFromPost( $this->files['image10'], $path, $bucket );
 
             if( empty( $article->id ) )
             {
